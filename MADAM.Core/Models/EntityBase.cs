407ade6d0e24dce70de76dd901294209a86c74ae
FTWinston <ftwinston@gmail.com>
2014-09-30 01:20:27 +0100

    Messing with T4 generation, with so-far questionable results.

--- MADAM.Core/Models/EntityBase.cs
+++ MADAM.Core/Models/EntityBase.cs
@@ -12,5 +12,33 @@ namespace MADAM.Core.Models
         public ThingType ThingType { get { return Thing.ThingType; } }
         public long ID { get { return Thing.ID; } }
 
+        /*
+        protected EntityBase(ThingType type)
+        {
+            ThingType = type;
+        }
+
+        protected EntityBase(Thing thing)
+        {
+            Thing = thing;
+        }*/
+
+        protected IList<IFieldValue> FieldValues;
+        /*
+        protected IList<FieldValues_Int> IntValues;
+        */
+        protected int GetIntValue(int fieldID)
+        {
+            var fieldVal = FieldValues.FirstOrDefault(v => v.FieldID == fieldID) as FieldValues_Int;
+            if (fieldVal != null)
+                return fieldVal.Value;
+
+            /*
+            var fieldVal = IntValues.FirstOrDefault(v => v.FieldID == fieldID);
+            if (fieldVal != null)
+                return fieldVal.Value;
+            */
+            return default(int);
+        }
     }
 }
